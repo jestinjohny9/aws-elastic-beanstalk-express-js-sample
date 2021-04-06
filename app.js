976a9810c120c4dd5a0ce6a8c80489655e5c25af
jestinmj <jestinmj88@gmail.com>
2021-04-06 15:48:32 +1200

    Testing more using Manual Review

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello AWS from HSL with the Review Step added!'));
+app.get('/', (req, res) => res.send('Hello AWS from HSL with the Review Step added!'));
+app.get('/', (req, res) => res.send('Hello AWS from HSL with the Review Step added!'));
+app.get('/', (req, res) => res.send('Hello AWS from HSL with the Review Step added!'));
+app.get('/', (req, res) => res.send('Hello AWS from HSL with the Review Step added!'));
+app.get('/', (req, res) => res.send('Hello AWS from HSL with the Review Step added!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
